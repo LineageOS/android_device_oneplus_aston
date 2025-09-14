9f6c1992484347dd4c7da198c4bae4fd11615a07
inferno0230 <mail@inferno0230.in>
2025-09-14 20:47:54 +0530

    aston: Skip oplus dimlayer zorder
    Change-Id: I2a9656d97cd3423004705f81cdf5b61aa9448703
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ TARGET_SCREEN_WIDTH := 1264
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946607878435459.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946607878435459.xml
 
+# Fingerprint
+$(call soong_config_set,OPLUS_LINEAGE_UDFPS_EXT,SKIP_OPLUS_DIMLAYER_ZORDER,true)
+
 # IR
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.consumerir.xml
